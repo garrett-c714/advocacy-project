2bde24d9804967abbdcdca7165a528a9ab027f9e
Garrett Cox <gcox5714@gmail.com>
2021-04-27 14:22:22 -0400

    Working on the Header 💊

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = props => {
     return (
         <div className='header-block flex-center'>
-            <p>Test</p>
+            <p className='header-header'>Alcohol and its effect on teens</p>
         </div>
     );
 }
