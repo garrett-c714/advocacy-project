cf50f303b7bf6064ca88b8fc97e643a521de5e56
Garrett Cox <gcox5714@gmail.com>
2021-04-27 14:10:23 -0400

    First Commit 🍺

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = props => {
+    return (
+        <div className='header-block flex-center'>
+            <p>Test</p>
+        </div>
+    );
+}
+
+export default Header;
